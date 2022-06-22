c7b4e9a12cf3119cbaf53be43a4af545a4fc0cac
Bruna Leal Torres Silva <53224985+bruninhaltorres@users.noreply.github.com>
2022-06-22 17:35:48 -0300

    Tentando conexão com o Banco de Dados

--- main.go
+++ main.go
@@ -1,15 +1,49 @@
 package main
 
-import ("net/http") //responsável por nos fornecer os métodos de mapeamento e gerenciamento de requisições
+import (
+	"net/http" //responsável por nos fornecer os métodos de mapeamento e gerenciamento de requisições
+	"fmt"
+	"database/sql" //fornece apenas uma interface leve sobre SQL. Deve ser usado em conjunto com um driver de banco de dados
+	// _ "github.com/go-sql-driver/mysql" //Driver
+) 
 
-func handler(w http.ResponseWriter, r *http.Request) {
+type Card struct {
+	Num_card    int
+	Cod_seg     int
+	Name        string
+	Date_venc   string
+	Status      string
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
 	//funçao que vai lidar com as requisições e respostas do servidor
 	//(resposta da requisição, tratamento da mesma)
+	db := dbConn() //abre a conexão com o banco de dados
 
 	http.ServeFile(w, r, "./static/index.html") //servindo uma página web
+
+	defer db.Close() //fecha a conexão
+}
+
+func dbConn() (db *sql.DB) {
+	dbDriver := "mysql"
+	dbUser   := "grupo8"
+	dbPass   := "123456"
+	dbName   := "OxeBanking"
+
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
 }
 
 func main() {
-	http.HandleFunc("/", handler) //(caminho, ação)
+	fmt.Println("Server started on: http://localhost:8080/")
+
+	http.HandleFunc("/", Index) //(caminho, ação)
+
 	http.ListenAndServe(":8080", nil) //especifica em qual porta rodará a aplicaçao e o nil nesse caso informa que utilizaremos a configuração padrão do servidor do Go
 }
