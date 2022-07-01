f5704353653dcd7ec085ab47c2e19a3545aeff7d
Bruna Torres <blts@ic.ufal.br>
2022-06-30 23:43:37 -0300

    Tabelas criadas

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `card` (
-    `num_card`    int(16)      NOT NULL     unsigned    AUTO_INCREMENT,
-    `cod_seg`     int(3)       NOT NULL     unsigned    AUTO_INCREMENT,
-    `name`        varchar(30)  NOT NULL,
-    `date_venc`   varchar(10)  NOT NULL,
-    `status`      varchar(10)  NOT NULL,
+create table card (
+    num_card    int(16)      unsigned NOT NULL AUTO_INCREMENT,
+    cod_seg     int(3)       unsigned NOT NULL,
+    name        varchar(30)  NOT NULL,
+    date_venc   varchar(10)  NOT NULL,
+    status      varchar(10)  NOT NULL,
     PRIMARY KEY   (`num_card`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=UTF-8;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http" //responsável por nos fornecer os métodos de mapeamento e gerenciamento de requisições
 	"fmt"
+	"text/template" // Gerencia templates
 	"database/sql" //fornece apenas uma interface leve sobre SQL. Deve ser usado em conjunto com um driver de banco de dados
 	_ "github.com/go-sql-driver/mysql" //Driver
 ) 
@@ -18,20 +19,55 @@ type Card struct {
 func Index(w http.ResponseWriter, r *http.Request) {
 	//funçao que vai lidar com as requisições e respostas do servidor
 	//(resposta da requisição, tratamento da mesma)
+
 	db := dbConn() //abre a conexão com o banco de dados
 
-	http.ServeFile(w, r, "./static/index.html") //servindo uma página web
+	// Realiza a consulta com banco de dados e trata erros
+	selDB, err := db.Query("SELECT * FROM card")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Monta a struct para ser utilizada no template
+	structCard := Card{}
+
+	// Monta um array para guardar os valores da struct
+	res := []Card{}
+
+	// Pega todos os valores do banco
+	for selDB.Next() {
+		// Armazena os valores em variáveis
+		var num_card, cod_seg int
+		var name, date_venc, status string
+
+		// Faz o Scan do SELECT
+		err = selDB.Scan(&num_card, &cod_seg, &name, &date_venc, &status)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		// Envia os resultados para a struct
+		structCard.Num_card = num_card
+		structCard.Cod_seg = cod_seg
+		structCard.Name = name
+		structCard.Date_venc = date_venc
+		structCard.Status = status
+
+		// Junta a Struct com Array
+		res = append(res, structCard)
+	}
+
+	// Abre a página Index e exibe todos os registrados na tela
+	tmpl.ExecuteTemplate(w, "Index", res)
+
+	// http.ServeFile(w, r, "./static/index.html") //servindo uma página web
 
 	defer db.Close() //fecha a conexão
 }
 
 func dbConn() (db *sql.DB) {
-	dbDriver := "mysql"
-	dbUser   := "root"
-	dbPass   := "123456"
-	dbName   := "OxeBanking"
 
-	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/cardService")
 
 	if err != nil {
 		panic(err.Error())
@@ -39,6 +75,8 @@ func dbConn() (db *sql.DB) {
 
 	return db
 }
+
+var tmpl = template.Must(template.ParseGlob("tmpl/*"))
 
 func main() {
 	fmt.Println("Server started on: http://localhost:8080/")
