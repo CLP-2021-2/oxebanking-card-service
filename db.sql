87325ce6f9bc9af7b1fbdfe01e4cfacde0a127ff
Bruna Torres <blts@ic.ufal.br>
2022-07-01 00:50:09 -0300

    Listagem na tela funcionando

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
 create table card (
-    num_card    int(16)      unsigned NOT NULL AUTO_INCREMENT,
+    id    int(16)      unsigned NOT NULL AUTO_INCREMENT,
     cod_seg     int(3)       unsigned NOT NULL,
     name        varchar(30)  NOT NULL,
     date_venc   varchar(10)  NOT NULL,
     status      varchar(10)  NOT NULL,
-    PRIMARY KEY   (`num_card`)
+    PRIMARY KEY   (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
--- main.go
+++ main.go
@@ -9,12 +9,25 @@ import (
 ) 
 
 type Card struct {
-	Num_card    int
+	Id    int
 	Cod_seg     int
 	Name        string
 	Date_venc   string
 	Status      string
 }
+
+func dbConn() (db *sql.DB) {
+
+	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/cardService")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
+
+var tmpl = template.Must(template.ParseGlob("tmpl/*"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	//funçao que vai lidar com as requisições e respostas do servidor
@@ -37,17 +50,17 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	// Pega todos os valores do banco
 	for selDB.Next() {
 		// Armazena os valores em variáveis
-		var num_card, cod_seg int
+		var id, cod_seg int
 		var name, date_venc, status string
 
 		// Faz o Scan do SELECT
-		err = selDB.Scan(&num_card, &cod_seg, &name, &date_venc, &status)
+		err = selDB.Scan(&id, &cod_seg, &name, &date_venc, &status)
 		if err != nil {
 			panic(err.Error())
 		}
 
 		// Envia os resultados para a struct
-		structCard.Num_card = num_card
+		structCard.Id = id
 		structCard.Cod_seg = cod_seg
 		structCard.Name = name
 		structCard.Date_venc = date_venc
@@ -65,23 +78,57 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	defer db.Close() //fecha a conexão
 }
 
-func dbConn() (db *sql.DB) {
+// Função Show exibe apenas um resultado
+func Show(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
 
-	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/cardService")
+	// Pega o ID do parametro da URL
+	id := r.URL.Query().Get("id")
 
+	// Usa o ID para fazer a consulta e tratar erros
+	selDB, err := db.Query("SELECT * FROM card WHERE id=?", id)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	return db
-}
+	// Monta a strcut para ser utilizada no template
+	structCard := Card{}
 
-var tmpl = template.Must(template.ParseGlob("tmpl/*"))
+	// Realiza a estrutura de repetição pegando todos os valores do banco
+	// Pega todos os valores do banco
+	for selDB.Next() {
+		// Armazena os valores em variáveis
+		var id, cod_seg int
+		var name, date_venc, status string
+
+		// Faz o Scan do SELECT
+		err = selDB.Scan(&id, &cod_seg, &name, &date_venc, &status)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		// Envia os resultados para a struct
+		structCard.Id = id
+		structCard.Cod_seg = cod_seg
+		structCard.Name = name
+		structCard.Date_venc = date_venc
+		structCard.Status = status
+
+	}
+
+	// Mostra o template
+	tmpl.ExecuteTemplate(w, "Show", structCard)
+
+	// Fecha a conexão
+	defer db.Close()
+
+}
 
 func main() {
 	fmt.Println("Server started on: http://localhost:8080/")
 
 	http.HandleFunc("/", Index) //(caminho, ação)
+	http.HandleFunc("/show", Show)
 
 	http.ListenAndServe(":8080", nil) //especifica em qual porta rodará a aplicaçao e o nil nesse caso informa que utilizaremos a configuração padrão do servidor do Go
 }
